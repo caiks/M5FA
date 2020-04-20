d8aa75981e75f6b3f63484af8de4fbd532d65cc2
Cliff McCartney <caiks@greenlake.co.uk>
2020-04-20 17:48:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -95,6 +95,9 @@ int main(int argc, char **argv)
 	if (argc >= 5 && string(argv[1]) == "induce_category_store")
 	{
 		auto uvars = systemsSetVar;
+		auto frvars = fudRepasSetVar;
+		auto frder = fudRepasDerived;
+		auto frund = fudRepasUnderlying;
 		auto applicationer = parametersSystemsHistoryRepasApplicationerMaxRollByMExcludedSelfHighestFmaxIORepa;
 
 		string model = string(argv[2]);
@@ -133,7 +136,60 @@ int main(int argc, char **argv)
 		systemRepasPersistent(*ur, out); cout << endl;
 		applicationRepasPersistent(*dr, out); cout << endl;
 		out.close();
+		EVAL(treesSize(*dr->slices));
+		EVAL(frder(*dr->fud)->size());
+		EVAL(frund(*dr->fud)->size());
+		EVAL(frvars(*dr->fud)->size());
+	}
+	
+		if (argc >= 5 && string(argv[1]) == "induce_category_store2")
+	{
+		auto uvars = systemsSetVar;
+		auto frvars = fudRepasSetVar;
+		auto frder = fudRepasDerived;
+		auto frund = fudRepasUnderlying;
+		auto applicationer = parametersSystemsHistoryRepasApplicationerMaxRollByMExcludedSelfHighestFmaxIORepa;
 
+		string model = string(argv[2]);
+		string cat_id = string(argv[3]);
+		string store_id = string(argv[4]);
+
+		auto xx = trainBucketedCategoryStoreIO(10, cat_id, store_id);
+		auto& uu = std::get<0>(xx);
+		auto& ur = std::get<1>(xx);
+		auto& hr = std::get<2>(xx);
+
+		auto vv = *uvars(*uu);
+
+		EVAL(hr->dimension);
+		EVAL(hr->size);
+
+		auto& vvi = ur->mapVarSize();
+		auto vv0 = sorted(vv);
+		SizeList vv1;
+		for (auto& v : vv0)
+			vv1.push_back(vvi[v]);
+
+		size_t wmax = 18;
+		size_t lmax = 20;
+		size_t xmax = 128 * 8;
+		size_t omax = 30;
+		size_t bmax = 30 * 3;
+		size_t mmax = 3;
+		size_t umax = 128;
+		size_t pmax = 1;
+		size_t fmax = 127;
+		size_t mult = 1;
+		size_t seed = 5;
+		auto dr = applicationer(wmax, lmax, xmax, omax, bmax, mmax, umax, pmax, fmax, mult, seed, vv1, *hr, *ur);
+		std::ofstream out(model+".bin", std::ios::binary);
+		systemRepasPersistent(*ur, out); cout << endl;
+		applicationRepasPersistent(*dr, out); cout << endl;
+		out.close();
+		EVAL(treesSize(*dr->slices));
+		EVAL(frder(*dr->fud)->size());
+		EVAL(frund(*dr->fud)->size());
+		EVAL(frvars(*dr->fud)->size());
 	}
 
 	return 0;
