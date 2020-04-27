dccc8b245965d27aca0b72c9c02bd67a95fabd29
Cliff McCartney <caiks@greenlake.co.uk>
2020-04-27 16:59:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -313,6 +313,7 @@ int main(int argc, char **argv)
 		EVAL(frund(*dr->fud)->size());
 		EVAL(frvars(*dr->fud)->size());
 	}
+
 	if (argc >= 5 && string(argv[1]) == "model_error_category_store")
 	{
 		auto uvars = systemsSetVar;
@@ -423,6 +424,117 @@ int main(int argc, char **argv)
 		}
 		err /= records->size();
 		EVAL(err);
+	}
+	
+	if (argc >= 5 && string(argv[1]) == "condition_category_store")
+	{
+		auto uvars = systemsSetVar;
+		auto hrsel = eventsHistoryRepasHistoryRepaSelection_u;
+		auto hrjoin = vectorHistoryRepasJoin_u;
+		auto frmul = historyRepasFudRepasMultiply_u;
+		auto frvars = fudRepasSetVar;
+		auto frder = fudRepasDerived;
+		auto frund = fudRepasUnderlying;
+		
+		string model = string(argv[2]);
+		string cat_id = string(argv[3]);
+		string store_id = string(argv[4]);
+		size_t dmult = argc >= 6 ? atoi(argv[5]) : 1;
+
+		auto xx = trainBucketedCategoryStoreIO(10, cat_id, store_id);
+		auto& uu = std::get<0>(xx);
+		auto& ur = std::get<1>(xx);
+		auto& hr0 = std::get<2>(xx);
+
+		auto vv = *uvars(*uu);
+		
+		// std::unique_ptr<HistoryRepa> hr;
+		// {
+			// SizeList ll;
+			// for (size_t j = 0; j < scale; j++)
+				// for (size_t i = 0; i < hr0->size; i++)
+					// ll.push_back(i);
+			// hr = hrsel(ll.size(), ll.data(), *hr0);
+		// }
+
+		// EVAL(hr->dimension);
+		// EVAL(hr->size);
+
+		// auto& vvi = ur->mapVarSize();
+		// auto vv0 = sorted(vv);
+		// SizeList vv1;
+		// for (auto& v : vv0)
+			// vv1.push_back(vvi[v]);
+		
+		// StrVarPtrMap m;
+		// std::ifstream in(model+".bin", std::ios::binary);
+		// auto ur1 = persistentsSystemRepa(in, m);
+		// auto dr = persistentsApplicationRepa(in);
+		// in.close();
+
+		// EVAL(treesSize(*dr->slices));
+		// EVAL(treesLeafElements(*dr->slices)->size());
+		// EVAL(frder(*dr->fud)->size());
+		// EVAL(frund(*dr->fud)->size());
+		// EVAL(frvars(*dr->fud)->size());
+
+		// auto hr1 = frmul(*hr, *dr->fud);
+		// if (hr1->evient)
+			// hr1->transpose();
+		// auto z = hr1->size;
+		// auto& mvv = hr1->mapVarInt();
+		// auto sl = treesLeafElements(*dr->slices);
+		// std::map<std::size_t, SizeList> slevs;
+		// std::vector<std::size_t> evsl;
+		// evsl.reserve(z);
+		// for (auto s : *sl)
+		// {
+			// auto pk = mvv[s];
+			// for (std::size_t j = 0; j < z; j++)
+			// {
+				// std::size_t u = hr1->arr[pk*z + j];
+				// if (u)
+				// {
+					// slevs[s].push_back(j);
+					// evsl.push_back(s);
+				// }
+			// }
+		// }
+// //		EVAL(slevs);
+// //		EVAL(evsl);
+		// std::vector<double> daily_error;
+		// for (auto& record : *records)
+		// {
+			// double e = 0.0; 
+			// for (std::size_t j = 0; j < z/scale-1; j++)
+				// e += (record.d[j+1] - record.d[j])*(record.d[j+1] - record.d[j]);
+			// e /= z/scale-1;
+			// daily_error.push_back(std::sqrt(e));
+		// }
+		// std::vector<double> slice_error;
+		// for (auto& record : *records)
+		// {
+			// double e = 0.0; 
+			// for (std::size_t j = 0; j < z; j++)
+			// {
+				// double a = 0.0;
+				// auto ll = slevs[evsl[j]];
+				// for (auto k : ll)
+					// a += record.d[k % (z/scale)];
+				// a /= ll.size();
+				// e += (record.d[j % (z/scale)] - a)*(record.d[j % (z/scale)] - a);
+			// }
+			// e /= z;
+			// slice_error.push_back(std::sqrt(e));
+		// }
+		// auto err = 0.0;
+		// for (std::size_t i = 0; i < records->size(); i++)
+		// {
+			// std::cout << (*records)[i].id << ":\t" << slice_error[i] << "\t" << daily_error[i] << "\t" << slice_error[i]/daily_error[i] << std::endl;
+			// err += slice_error[i]/daily_error[i];
+		// }
+		// err /= records->size();
+		// EVAL(err);
 	}
 
 	return 0;
